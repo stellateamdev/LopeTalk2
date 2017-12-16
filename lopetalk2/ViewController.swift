ccf247a5ece08ec4188383f334df640c1de652bd
mrkyrp <mrkyrp@gmail.com>
2017-12-16 18:39:56 +0700

    Initial Commit

---
+++ lopetalk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lopetalk2
+//
+//  Created by marky RE on 16/12/2560 BE.
+//  Copyright © 2560 marky RE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
